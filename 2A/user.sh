e473e28287b23e16b13a559191c296a2fdabd8a3
Aniket Patil <aniketpatil2027@gmail.com>
2026-02-08 16:35:45 +0530

    add

---
+++ 2A/user.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Enter your name: " name
+echo "Welcome $name, to the world of bash scripting" 
+
+sudo useradd -m $username 
+echo "New user added"
---
+++ 2A/vairable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+# This is the jethalal ki duniya 
+
+<< comment
+This is the multi line comment
+comment
+
+name="jethalal"
+echo "My name is $name, and date is $(date)" 
+
+echo "Enter your name: "
+read name 
+echo "Welcome $name, to the world of bash scripting"
